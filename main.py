ed9eec1f723178533da7ed63cb1d90563e0a1da1
fazerog02 <fazerog02@gmail.com>
2022-01-11 03:54:20 +0900

    [fix]classを使わないように修正

--- main.py
+++ main.py
@@ -4,109 +4,123 @@ import pygame
 from pygame.locals import *
 
 
-apple_score = 0
-banana_score = 0
-drop_count = 0
-
-
-class Box(pygame.sprite.Sprite):
-    def __init__(self, fruits):
-        pygame.sprite.Sprite.__init__(self, self.containers)
-        self.image = pygame.image.load("./images/box.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.left = pygame.display.get_surface().get_rect().centerx
-        self.rect.top = pygame.display.get_surface().get_rect().centery + 200
-        self.fruits = fruits
-        self.update = self.move
-    
-    def move(self):
-        self.rect.left, self.rect.top = pygame.mouse.get_pos()
-        self.rect.left -= self.rect.width / 2
-        self.rect.top -= self.rect.height / 2
-        collided_fruits = pygame.sprite.spritecollide(self, self.fruits, True)
-        for fruit in collided_fruits:
-            if fruit.type == "apple":
-                global apple_score
-                apple_score += 1
-            elif fruit.type == "banana":
-                global banana_score
-                banana_score += 1
-            pygame.mixer.music.load("./audio/catch.ogg")
-            pygame.mixer.music.play(1)
-
-
-class Fruit(pygame.sprite.Sprite):
-    def __init__(self, type):
-        pygame.sprite.Sprite.__init__(self, self.containers)
-        self.type = type
-        self.image = pygame.image.load(f"./images/{type}.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.left = randint(0, pygame.display.get_surface().get_size()[0])
-        self.rect.top = 0
-        self.speed = randint(3, 5)
-        self.update = self.fall
-    
-    def fall(self):
-        self.rect.top += self.speed
-        if self.rect.top >= pygame.display.get_surface().get_size()[1]:
-            global drop_count
-            drop_count += 1
-            pygame.mixer.music.load("./audio/drop.ogg")
-            pygame.mixer.music.play(1)
-            self.kill()
-            return
-        
-
 def main():
     pygame.init()
     pygame.mixer.init()
 
-    screen = pygame.display.set_mode((0, 0), RESIZABLE)
-    splite_controller = pygame.sprite.RenderUpdates()
+    gamen = pygame.display.set_mode((0, 0))
     clock = pygame.time.Clock()
+    score_font = pygame.font.SysFont(None, 40)
 
-    global apple_score
-    global banana_score
-    global drop_count
+    apple_image = pygame.image.load("./images/apple.png").convert_alpha()
+    banana_image = pygame.image.load("./images/banana.png").convert_alpha()
+    box_image = pygame.image.load("./images/box.png").convert_alpha()
 
-    fruits = pygame.sprite.Group()
-    Fruit.containers = splite_controller, fruits
+    gamen_yokohaba = pygame.display.get_surface().get_size()[0]
+    gamen_tatehaba = pygame.display.get_surface().get_size()[1]
 
-    Box.containers = splite_controller
-    Box(fruits)
+    fruit_size = [50, 50]
+    box_size = [150, 100]
 
-    sysfont = pygame.font.SysFont(None, 40)
+    box_basyo = [int(gamen_yokohaba/2), int(gamen_tatehaba/2)]
+
+    apple_score = 0
+    banana_score = 0
+    drop_count = 0
+
+    apple_list = []
+    for i in range(3):
+        yoko = randint(0, gamen_yokohaba)
+        tate = 0
+        speed = randint(2, 4)
+        apple_list.append([yoko, tate, speed])
+    banana_list = []
+    for i in range(3):
+        yoko = randint(0, gamen_yokohaba)
+        tate = 0
+        speed = randint(2, 4)
+        banana_list.append([yoko, tate, speed])
+
+    tyouten_list = [
+        [0, 0],
+        [fruit_size[0], 0],
+        [0, fruit_size[1]],
+        [fruit_size[0], fruit_size[1]],
+    ]
 
     while True:
         clock.tick(60)
-        screen.fill((255, 255, 255))
+        gamen.fill((255, 255, 255))
 
-        if randint(1, 30) == 1:
-            if randint(0, 1) == 0:
-                Fruit("apple")
-            else:
-                Fruit("banana")
+        box_basyo[0] = pygame.mouse.get_pos()[0] - int(box_size[0]/2)
+        box_basyo[1] = pygame.mouse.get_pos()[1] - int(box_size[1]/2)
+        gamen.blit(box_image, (box_basyo[0], box_basyo[1]))
 
-        splite_controller.update()
-        splite_controller.draw(screen)
+        for apple in apple_list:
+            apple[1] += apple[2]
+            gamen.blit(apple_image, (apple[0]-int(fruit_size[0]/2), apple[1]-int(fruit_size[1]/2)))
+            for tyouten in tyouten_list:
+                tyouten_yoko = apple[0] + tyouten[0]
+                tyouten_tate = apple[1] + tyouten[1]
+                if box_basyo[0] <= tyouten_yoko <= box_basyo[0]+box_size[0] and box_basyo[1] <= tyouten_tate <= box_basyo[1]+box_size[1]:
+                    apple_score += 1
+                    apple[0] = randint(0, gamen_yokohaba)
+                    apple[1] = 0
+                    pygame.mixer.music.load("./audio/catch.ogg")
+                    pygame.mixer.music.play(1)
+                elif apple[1] >= gamen_tatehaba:
+                    drop_count += 1
+                    apple[0] = randint(0, gamen_yokohaba)
+                    apple[1] = 0
+                    pygame.mixer.music.load("./audio/drop.ogg")
+                    pygame.mixer.music.play(1)
+
+        for banana in banana_list:
+            banana[1] += banana[2]
+            gamen.blit(banana_image, (banana[0]-int(fruit_size[0]/2), banana[1]-int(fruit_size[1]/2)))
+            for tyouten in tyouten_list:
+                tyouten_yoko = banana[0] + tyouten[0]
+                tyouten_tate = banana[1] + tyouten[1]
+                if box_basyo[0] <= tyouten_yoko <= box_basyo[0]+box_size[0] and box_basyo[1] <= tyouten_tate <= box_basyo[1]+box_size[1]:
+                    banana_score += 1
+                    banana[0] = randint(0, gamen_yokohaba)
+                    banana[1] = 0
+                    pygame.mixer.music.load("./audio/catch.ogg")
+                    pygame.mixer.music.play(1)
+                elif banana[1] >= gamen_tatehaba:
+                    drop_count += 1
+                    banana[0] = randint(0, gamen_yokohaba)
+                    banana[1] = 0
+                    pygame.mixer.music.load("./audio/drop.ogg")
+                    pygame.mixer.music.play(1)
 
         score_txt = "drop: " + str(drop_count)
-        text = sysfont.render(score_txt, True, (0, 0, 0))
-        screen.blit(text, (50, 50))
+        text = score_font.render(score_txt, True, (0, 0, 0))
+        gamen.blit(text, (50, 50))
 
         score_txt = "apple: " + str(apple_score)
-        text = sysfont.render(score_txt, True, (0, 0, 0))
-        screen.blit(text, (50, 90))
+        text = score_font.render(score_txt, True, (0, 0, 0))
+        gamen.blit(text, (50, 90))
 
         score_txt = "banana: " + str(banana_score)
-        text = sysfont.render(score_txt, True, (0, 0, 0))
-        screen.blit(text, (50, 130))
-        
-        if drop_count >= 3:
-            pygame.quit()
-            sys.exit()
+        text = score_font.render(score_txt, True, (0, 0, 0))
+        gamen.blit(text, (50, 130))
+
+        if drop_count >= 5:
+            break
 
         pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+    message_font = pygame.font.SysFont(None, 100)
+    while True:
+        clock.tick(60)
+        gamen.fill((255, 255, 255))
+        text = message_font.render("Game Over!", True, (0, 0, 0))
+        gamen.blit(text, (gamen_yokohaba/2, gamen_yokohaba/2))
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
